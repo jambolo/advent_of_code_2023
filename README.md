c6a46a66966b3f50129922a5c4748167b1f21f2a
John Bolton <jambolo@users.noreply.github.com>
2023-12-22 13:14:42 -1000

    Day 3

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ My solutions for Advent of Code 2023 implemented in Rust
 ### Day 1
 
 First introduction to Rust. My first impression is that the pervasive use of Option<> makes even the most trivial tasks, such as this one, very complicated.
+
+### Day 2
+
+Copilot is turning out to be relatively useful. It is pretty good at doing the boilerplate code and copy pasta. The boilerplate code is  especially helpful while Rust is new to me. I don't have to worry about all of the minute details.
+
+### Day 3
+
+Collections in Rust are pretty straightforward. Objects are still a bit of a mystery. 
---
+++ day3/src/main.rs
@@ -0,0 +1,149 @@
+use std::env;
+use common::read_lines;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+
+// Returns true if the character is a symbol
+fn is_symbol(ch: char) -> bool {
+    ch != '.' && !ch.is_ascii_digit()
+}
+
+// Returns true if the character is a gear
+fn is_gear(ch: char) -> bool {
+    ch == '*'
+}
+
+// Returns true if the character adjacent to a symbol
+fn is_adjacent_to_symbol(grid: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
+    if x > 0 {
+        if is_symbol(grid[y][x-1]) {
+            return true;
+        }
+        if y > 0 && is_symbol(grid[y-1][x-1]) {
+            return true;
+        }
+        if y < grid.len() - 1 && is_symbol(grid[y+1][x-1]) {
+            return true;
+        }
+    }
+
+    if y > 0 && is_symbol(grid[y-1][x]) {
+        return true;
+    }
+    if y < grid.len() - 1 && is_symbol(grid[y+1][x]) {
+        return true;
+    }
+
+    if x < grid[y].len() - 1 {
+        if is_symbol(grid[y][x+1]) {
+            return true;
+        }
+        if y > 0 && is_symbol(grid[y-1][x+1]) {
+            return true;
+        }
+        if y < grid.len() - 1 && is_symbol(grid[y+1][x+1]) {
+            return true;
+        }
+    }
+
+    false
+}
+
+// Returns true if the character adjacent to a symbol
+fn find_adjacent_gears(grid: &Vec<Vec<char>>, x: usize, y: usize, gears: &mut BTreeSet<(usize, usize)>) {
+    if x > 0 {
+        if is_gear(grid[y][x-1]) {
+            gears.insert((x-1, y));
+        }
+        if y > 0 && is_gear(grid[y-1][x-1]) {
+            gears.insert((x-1, y-1));
+        }
+        if y < grid.len() - 1 && is_gear(grid[y+1][x-1]) {
+            gears.insert((x-1, y+1));
+        }
+    }
+
+    if y > 0 && is_gear(grid[y-1][x]) {
+        gears.insert((x, y-1));
+    }
+    if y < grid.len() - 1 && is_gear(grid[y+1][x]) {
+        gears.insert((x, y+1));
+    }
+
+    if x < grid[y].len() - 1 {
+        if is_gear(grid[y][x+1]) {
+            gears.insert((x+1, y));
+        }
+        if y > 0 && is_gear(grid[y-1][x+1]) {
+            gears.insert((x+1, y-1));
+        }
+        if y < grid.len() - 1 && is_gear(grid[y+1][x+1]) {
+            gears.insert((x+1, y+1));
+        }
+    }
+}
+
+fn scan_number(grid: &Vec<Vec<char>>, x0: usize, y0: usize) -> (usize, u32, bool, BTreeSet<(usize, usize)>) {
+    let mut xn = x0;
+    let mut value = 0;
+    let mut is_part_number = false;
+    let mut adjacent_gears: BTreeSet<(usize, usize)> = BTreeSet::new();
+    while xn < grid[y0].len() && grid[y0][xn].is_ascii_digit() {
+        value = value * 10 + grid[y0][xn].to_digit(10).unwrap();
+        if is_adjacent_to_symbol(grid, xn, y0) {
+            is_part_number = true;
+            find_adjacent_gears(grid, xn, y0, &mut adjacent_gears);
+        }
+        xn = xn + 1;
+    }
+    (xn, value, is_part_number, adjacent_gears)
+}
+
+fn main() {
+    // Get path to data
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+
+    // Load data
+    let lines = read_lines(path).unwrap();
+
+    // Create a 2D array of characters
+    let mut grid: Vec<Vec<char>> = Vec::new();
+
+    for s in lines {
+        grid.push(s.chars().collect());
+    }
+
+    let mut sum = 0;
+    let mut gears: BTreeMap<(usize, usize), Vec<u32>> = BTreeMap::new();
+
+    // Scan the grid for part numbers
+    for y in 0 .. grid.len() {
+        let mut x = 0;
+        while x < grid[y].len() {
+            if grid[y][x].is_ascii_digit() {
+                let (new_x, value, is_part_number, adjacent_gears) = scan_number(&grid, x, y);
+                if is_part_number {
+                    sum += value;
+                    for g in adjacent_gears {
+                        gears.entry(g).or_insert(Vec::new()).push(value);
+                    }
+                }
+                x = new_x;
+            }
+            x = x + 1;
+        }
+    }
+
+    println!("Sum: {}", sum);
+
+    let mut gear_ratio_sum: u64 = 0;
+    for g in gears {
+        println!("Gears at ({}, {}): {:?}", g.0.0, g.0.1, g.1);
+        if g.1.len() == 2 {
+            gear_ratio_sum += g.1[0] as u64 * g.1[1] as u64;
+        }
+    }
+
+    println!("Gear ratio sum: {}", gear_ratio_sum);
+}
