ad267965ff48ac66fc7fd1bca075a2fcff8efd17
John Bolton <jambolo@users.noreply.github.com>
2023-12-28 11:45:38 -1000

    Day 11, part 2

--- README.md
+++ README.md
@@ -44,4 +44,8 @@ Again trivial. In Rust, you have to make a conscious effort to avoid making copi
 
 ### Day 10
 
-Pretty easy. It helps that I now how to determine if a point is inside a polygon.
+Pretty easy. It helps that I know how to determine if a point is inside a polygon.
+
+### Day 11
+
+Easy again. I wish Rust weren't so picky about integer types. I'm using `as` everywhere, so it is not really helping. Copilot has been really helpful by writing the glue and boiler plate code for me, and also showing how certain things can be implemented in Rust.
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,39 +1,49 @@
 use common::*;
 
+const EXPANSION: i64 = 1000000 - 1;
+
 fn main() {
-    let lines = load_data();
+    let galaxy = load_data();
 
-//    println!("Original galaxy:");
-//    draw_galaxy(&lines);
-
-    let galaxy = expand(&lines);
-
-//    println!("Expanded galaxy:");
-//    draw_galaxy(&galaxy);
-
+    let (xr, xc) = expand(&galaxy);
     let stars = find_stars(&galaxy);
-//    println!("Stars: {:?}", stars);
+    let distances = find_distances(&stars, &xr, &xc);
 
-    let distances = find_distances(&stars);
-//    println!("Distances: {:?}", distances);
-
-    println!("Sum of distances: {}", distances.iter().sum::<i32>());
+    println!("Sum of distances: {}", distances.iter().sum::<i64>());
 }
 
-fn find_distances(stars: &Vec<(usize, usize)>) -> Vec<i32> {
-    let mut distances: Vec<i32> = Vec::new();
+fn find_distances(stars: &Vec<(usize, usize)>, xr: &Vec<usize>, xc: &Vec<usize>) -> Vec<i64> {
+    let mut distances: Vec<i64> = Vec::new();
     for i in 0..stars.len() - 1 {
         for j in i + 1..stars.len() {
-            distances.push(distance(&stars[i], &stars[j]));
+            distances.push(distance(&stars[i], &stars[j], xr, xc));
         }
     }
     distances
 }
 
-fn distance(star1: &(usize, usize), star2: &(usize, usize)) -> i32 {
-    let dr = (star1.0 as isize - star2.0 as isize).abs();
-    let dc = (star1.1 as isize - star2.1 as isize).abs();
-    (dr + dc) as i32
+fn distance(star1: &(usize, usize), star2: &(usize, usize), xr: &Vec<usize>, xc: &Vec<usize>) -> i64 {
+    let min_r = star1.0.min(star2.0);
+    let max_r = star1.0.max(star2.0);
+    let min_c = star1.1.min(star2.1);
+    let max_c = star1.1.max(star2.1);
+    let row_expansion = number_of_expansions_between(xr, min_r, max_r) * EXPANSION;
+    let dr = max_r as i64 - min_r as i64 + row_expansion;
+    let column_expansion = number_of_expansions_between(xc, min_c, max_c) * EXPANSION;
+    let dc = (star1.1 as i64 - star2.1 as i64).abs() + column_expansion;
+    (dr + dc) as i64
+}
+
+
+fn number_of_expansions_between(vec: &Vec<usize>, a: usize, b: usize) -> i64 {
+    let start = match vec.binary_search(&a) {
+        Ok(pos) | Err(pos) => pos,
+    };
+    let end = match vec.binary_search(&b) {
+        Ok(pos) | Err(pos) => pos,
+    };
+//    println!("vec: {:?}, a: {}, b: {} => start: {}, end: {}", vec, a, b, start, end);
+    (end - start) as i64
 }
 
 fn find_stars(galaxy: &Vec<String>) -> Vec<(usize, usize)> {
@@ -47,7 +57,6 @@ fn find_stars(galaxy: &Vec<String>) -> Vec<(usize, usize)> {
     }
     stars
 }
-
 fn _draw_galaxy(galaxy: &Vec<String>) {
     for line in galaxy {
         println!("{}", line);
@@ -56,21 +65,21 @@ fn _draw_galaxy(galaxy: &Vec<String>) {
 }
 
 // Expands the galaxy
-fn expand(galaxy: &Vec<String>) -> Vec<String> {
-    expand_vertically(&expand_horizontally(galaxy))
+fn expand(galaxy: &Vec<String>) -> (Vec<usize>, Vec<usize>) {
+    let xr = expand_vertically(galaxy);
+    let xc = expand_horizontally(galaxy);
+    (xr, xc)
 }
 
 // Expands the galaxy horizontally
-fn expand_horizontally(galaxy: &Vec<String>) -> Vec<String> {
-    let mut new_galaxy: Vec<String> = galaxy.clone();
-    for i in (0..galaxy[0].len()).rev() {
+fn expand_horizontally(galaxy: &Vec<String>) -> Vec<usize> {
+    let mut xc: Vec<usize> = Vec::new();
+    for i in 0..galaxy[0].len() {
         if column_is_empty(galaxy, i) {
-            for line in &mut new_galaxy {
-                line.insert(i, '.');
-            }
+            xc.push(i);
         }
     }
-    new_galaxy
+    xc
 }
 
 // Returns true if the column is empty
@@ -83,14 +92,14 @@ fn column_is_empty(galaxy: &Vec<String>, column: usize) -> bool {
     true
 }
 
-fn expand_vertically(galaxy: &Vec<String>) -> Vec<String> {
-    let mut new_galaxy: Vec<String> = galaxy.clone();
-    for i in (0..galaxy.len()).rev() {
+fn expand_vertically(galaxy: &Vec<String>) -> Vec<usize> {
+    let mut xr: Vec<usize> = Vec::new();
+    for i in 0..galaxy.len() {
         if row_is_empty(&galaxy[i]) {
-            new_galaxy.insert(i, galaxy[i].clone());
+            xr.push(i);
         }
     }
-    new_galaxy
+    xr
 }
 
 // Returns true if the row is empty
