ae22dee757e4b7a9253d823809dc30d7258d10df
John Bolton <jambolo@users.noreply.github.com>
2024-11-15 23:18:30 -1000

    Day 16, part 1

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,38 +1,54 @@
 use std::{
     env,
-    fs::File,
     fs::read_to_string,
-    io::{self, BufRead, BufReader},
+    io,
     path::Path,
 };
 
 /// Loads lines of data from the file specified in the command into a vector of strings
 pub fn load_lines() -> Vec<String> {
-    // Get path
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
+    let path = get_path();
 
     // Load data
-    read_lines(path).expect(&format!("Could not read the file \"{}\"", path))
+    read_lines(&path).expect(&format!("Could not read the file \"{}\"", path))
 }
 
 /// Reads a file line by line into a vector of strings
 fn read_lines(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-    BufReader::new(File::open(filename)?).lines().collect()
+    let input = read_to_string(filename)?;
+    Ok(input.lines().map(|line| line.to_string()).collect())
 }
 
-/// Reads a file into a vector of strings separated by ','.
+/// Loads a file into a vector of strings separated by ','.
 pub fn load_comma_separated_values() -> Vec<String> {
-    // Get path
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
+    let path = get_path();
 
     // Load data
-    read_comma_separated_values(path).expect(&format!("Could not read the file \"{}\"", path))
+    read_comma_separated_values(&path).expect(&format!("Could not read the file \"{}\"", path))
 }
 
 /// Reads an entire file into a string and splits it by ',' into a vector of strings
 fn read_comma_separated_values(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     let input = read_to_string(filename)?;
     Ok(input.split(',').map(|s| s.trim().to_string()).collect())
+}
+
+/// Loads a file into a 2D array of characters
+pub fn load_map() -> Vec<Vec<char>> {
+    let path = get_path();
+
+    // Load data
+    read_map(&path).expect(&format!("Could not read the file \"{}\"", path))
+}
+
+/// Reads an entire file into a 2D array of characters
+fn read_map(filename: impl AsRef<Path>) -> io::Result<Vec<Vec<char>>> {
+    let input = read_to_string(filename)?;
+    Ok(input.lines().map(|line| line.chars().collect()).collect())
+}
+
+/// Gets the path from the command line arguments
+fn get_path() -> String {
+    let args: Vec<String> = env::args().collect();
+    args[1].clone()
 }
---
+++ day16/src/main.rs
@@ -0,0 +1,198 @@
+use common::*;
+
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    Right,
+    Up,
+    Left,
+    Down,
+}
+#[derive(Debug, Clone, Copy)]
+    struct Branch {
+        direction: Direction,
+        x: usize,
+        y: usize
+    }
+
+fn main() {
+    println!("Day 15, part 1");
+    let mut map = load_map();
+//    print_map(&map);
+
+    let mut energized: Vec<Vec<i32>> = vec![vec![0; map[0].len()]; map.len()];
+    let mut branches: Vec<Branch> = Vec::new();
+    branches.push(Branch { direction: Direction::Right, x: 0, y: 0 });
+    
+    // Move according to each branch in the stack until we have none left. Movement in a direction is done until we
+    // hit something or we reach the edge of the map. If we hit something, we stop moving in that direction and push
+    // 0, 1, or 2 new branches onto the stack depending on why we stopped.
+    while let Some(branch) = branches.pop() {
+        let x = branch.x;
+        let y = branch.y;
+        let direction = branch.direction;
+
+        match direction {
+            Direction::Right => move_right(&mut map, x, y, &mut branches, &mut energized),
+            Direction::Up => move_up(&mut map, x, y, &mut branches, &mut energized),
+            Direction::Left => move_left(&mut map, x, y, &mut branches, &mut energized),
+            Direction::Down => move_down(&mut map, x, y, &mut branches, &mut energized),
+        }
+    }
+    // Count the number of energized cells
+    let count: i32 = energized.iter().flatten().sum();
+    println!("Energized cells: {}", count);
+}
+
+fn move_right(map: &mut Vec<Vec<char>>, mut x: usize, y: usize, branches: &mut Vec<Branch>, energized: &mut Vec<Vec<i32>>) {
+    let right_edge = map[0].len() - 1;
+    let bottom_edge = map.len() - 1;
+
+    // Move right until we hit something or the edge
+    loop {
+        energized[y][x] = 1;
+        if map[y][x] != '.' && map[y][x] != '-' || x >= right_edge {
+            break;
+        }
+        x += 1;
+    }
+
+
+    // If we hit something, we need to branch unless we are at the edge
+    match map[y][x] {
+        '/' => branch_up(x, y, branches),
+        '\\' => branch_down(x, y, bottom_edge, branches),
+        '|' => {
+            map[y][x] = 'X';    // Prevent cycles
+            branch_up(x, y, branches);
+            branch_down(x, y, bottom_edge, branches);
+        },
+        'X' => {}, // Cycle detected, so we don't continue in any direction
+        '.' => {}, // Going off the edge, so we don't continue in any direction
+        '-' => {}, // Going off the edge, so we don't continue in any direction
+
+        _ => panic!("Unexpected character: {}", map[y][x]),
+    }
+}
+
+fn move_up(map: &mut Vec<Vec<char>>, x: usize, mut y: usize, branches: &mut Vec<Branch>, energized: &mut Vec<Vec<i32>>) {
+    let right_edge = map[0].len() - 1;
+    let _bottom_edge = map.len() - 1;
+
+    // Move up until we hit something or the edge
+    loop {
+        energized[y][x] = 1;
+        if map[y][x] != '.' && map[y][x] != '|' || y == 0 {
+            break;
+        }
+        y -= 1;
+    }
+
+    // If we hit something, we need to branch
+    match map[y][x] {
+        '/' =>  branch_right(x, y, right_edge, branches),
+        '\\' => branch_left(x, y, branches),
+        '-' => {
+            map[y][x] = 'X'; // Prevent cycles
+            branch_right(x, y, right_edge, branches);
+            branch_left(x, y, branches);
+        },
+        'X' => {}, // Cycle detected, so we don't continue in any direction
+        '.' => {}, // Going off the edge, so we don't continue in any direction
+        '|' => {}, // Going off the edge, so we don't continue in any direction
+
+        _ => panic!("Unexpected character: {}", map[y][x]),
+    }
+}
+
+fn move_left(map: &mut Vec<Vec<char>>, mut x: usize, y: usize, branches: &mut Vec<Branch>, energized: &mut Vec<Vec<i32>>) {
+    let _right_edge = map[0].len() - 1;
+    let bottom_edge = map.len() - 1;
+
+    // Move left until we hit something or the edge
+    loop {
+        energized[y][x] = 1;
+        if map[y][x] != '.' && map[y][x] != '-' || x == 0 {
+            break;
+        }
+        x -= 1;
+    }
+
+    // If we hit something, we need to branch
+    match map[y][x] {
+        '/' => branch_down(x, y, bottom_edge, branches),
+        '\\' => branch_up(x, y, branches),
+        '|' => {
+            map[y][x] = 'X'; // Prevent cycles
+            branch_down(x, y, bottom_edge, branches);
+            branch_up(x, y, branches);
+        },
+        'X' => {}, // Cycle detected, so we don't continue in any direction
+        '.' => {}, // Going off the edge, so we don't continue in any direction
+        '-' => {}, // Going off the edge, so we don't continue in any direction
+
+        _ => panic!("Unexpected character: {}", map[y][x]),
+    }
+}
+
+fn move_down(map: &mut Vec<Vec<char>>, x: usize, mut y: usize, branches: &mut Vec<Branch>, energized: &mut Vec<Vec<i32>>) {
+    let right_edge = map[0].len() - 1;
+    let bottom_edge = map.len() - 1;
+
+    // Move down until we hit something or the edge
+    loop {
+        energized[y][x] = 1;
+        if map[y][x] != '.' && map[y][x] != '|' || y >= bottom_edge {
+            break;
+        }
+        y += 1;
+    }
+
+    // If we hit something, we need to branch
+    match map[y][x] {
+        '/' => branch_left(x, y, branches),
+        '\\' => branch_right(x, y, right_edge, branches),
+        '-' => {
+            map[y][x] = 'X'; // Prevent cycles
+            branch_left(x, y, branches);
+            branch_right(x, y, right_edge, branches);
+        },
+        'X' => {}, // Cycle detected, so we don't continue in any direction
+        '.' => {}, // Going off the edge, so we don't continue in any direction
+        '|' => {}, // Going off the edge, so we don't continue in any direction
+
+        _ => panic!("Unexpected character: {}", map[y][x]),
+    }
+}
+
+fn branch_up( x: usize, y: usize, branches: &mut Vec<Branch>) {
+    if y > 0 {
+        branches.push(Branch { direction: Direction::Up, x, y: y - 1 })
+    }
+}
+
+fn branch_down(x: usize, y: usize, edge: usize, branches: &mut Vec<Branch>) {
+    if y < edge {
+        branches.push(Branch { direction: Direction::Down, x, y: y + 1 })
+    }
+}
+
+fn branch_left(x: usize, y: usize, branches: &mut Vec<Branch>) {
+    if x > 0 {
+        branches.push(Branch { direction: Direction::Left, x: x - 1, y })
+    }
+}
+
+fn branch_right(x: usize, y: usize, edge: usize, branches: &mut Vec<Branch>) {
+    if x < edge {
+        branches.push(Branch { direction: Direction::Right, x: x + 1, y })
+    }
+}
+
+fn _print_map(map: &Vec<Vec<char>>) {
+    for row in map {
+        for c in row {
+            print!("{}", c);
+        }
+        println!();
+    }
+}
